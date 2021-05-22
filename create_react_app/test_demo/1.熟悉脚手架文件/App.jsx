b555271457388ad2ef351084994ead1fdbd6b835
littleredhatli <1397063348@qq.com>
2021-05-22 18:01:06 +0800

    react v16.8基础部分

---
+++ create_react_app/test_demo/1.熟悉脚手架文件/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Hello from "./component/Hello/Hello"
+
+export default class App extends Component{
+  render(){
+    return(
+      <div>
+        <Hello/>
+      </div>
+    )
+  }
+}
---
+++ create_react_app/test_demo/1.熟悉脚手架文件/component/Hello/Hello.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <h1 className="title">Hello, React!</h1>
+    )
+  }
+}
